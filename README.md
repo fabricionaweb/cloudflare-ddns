cdd544dc18331c45505d34ebeb45de7df2edc279
Fabricio Silva <fabricionaweb@gmail.com>
2022-07-22 23:45:42 +0100

    Adds openwrt script

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # Cloudflare record update
 
-A small python script that updates a cloudflare dns record with your current ip.
+Small scripts to update cloudflare dns records with your current ip (Dynamic DNS).
 
-You need load the `.env` in memory before runs the `ddns.py`. The necessary values are inside `.env.sample`.
+You need load the `.env` in memory before runs the `ddns.py` (or hard code the values).
 
 You can register a cronjob to execute in an interval of choice.
 
-## docker
+# OpenWrt
 
-A tiny alpine image are builded with only the script inside. You only need to set the correct environment variables like in `.env.sample`
+Bash script to run on OpenWrt without dependencies.
 
-`docker run --init --rm --env-file=.env ghcr.io/fabricionaweb/cloudflare-ddns`
-
-## docker-compose
-
-You can simple run `docker compose up` if the environment variables are defined.
+I'll add more instructions about use it with [Hotplug](https://openwrt.org/docs/guide-user/base-system/hotplug)
---
+++ ddns.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# ---
+# settings
+ZONE="domain.com"
+RECORD="sub.domain.com"
+# you need to generate the API Token on dashboard
+# https://api.cloudflare.com/#getting-started-requests
+TOKEN=""
+
+# ---
+# cloudflare api
+ENDPOINT="https://api.cloudflare.com/client/v4"
+
+# ---
+# gets the ip from WAN
+# https://openwrt.org/docs/guide-developer/network-scripting#get_wan_address
+. /lib/functions/network.sh
+network_flush_cache
+network_find_wan NET_IF
+network_get_ipaddr NET_ADDR "${NET_IF}"
+echo "Current IP: ${NET_ADDR}"
+
+# ---
+# gets the domain zone id
+# https://api.cloudflare.com/#zone-list-zones
+ZONE_ID=$(curl -s -X GET "${ENDPOINT}/zones?name=${ZONE}" \
+  -H "Authorization: Bearer ${TOKEN}" \
+  -H "Content-Type: application/json" | jsonfilter -e "@.result[0].id")
+echo "Zone ID: ${ZONE_ID}"
+
+# ---
+# gets the dns record id
+# https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
+RECORD_ID=$(curl -s -X GET "${ENDPOINT}/zones/${ZONE_ID}/dns_records?name=${RECORD}" \
+  -H "Authorization: Bearer ${TOKEN}" \
+  -H "Content-Type: application/json" | jsonfilter -e "@.result[0].id")
+echo "Record ID: ${RECORD_ID}"
+
+# ---
+# updates the dns record
+# https://api.cloudflare.com/#dns-records-for-a-zone-patch-dns-record
+RESULT=$(curl -s -X PATCH "${ENDPOINT}/zones/${ZONE_ID}/dns_records/${RECORD_ID}" \
+  -H "Authorization: Bearer ${TOKEN}" \
+  -H "Content-Type: application/json" \
+  --data "{\"content\":\"${NET_ADDR}\"}" | jsonfilter -e "@.result")
+
+echo "Result: ${RESULT}"
