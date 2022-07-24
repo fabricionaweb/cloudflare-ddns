36c96b0bfb670f585a219f2cc8901d976c2eea35
Fabricio Silva <fabricionaweb@gmail.com>
2022-07-24 08:32:02 +0100

    Adds support to get remote ip when not in bridge mode

--- ddns.sh
+++ ddns.sh
@@ -13,12 +13,22 @@ TOKEN=""
 ENDPOINT="https://api.cloudflare.com/client/v4"
 
 # ---
-# gets the ip from WAN
+# gets the ip from WAN (works in bridge mode)
 # https://openwrt.org/docs/guide-developer/network-scripting#get_wan_address
-. /lib/functions/network.sh
-network_flush_cache
-network_find_wan NET_IF
-network_get_ipaddr NET_ADDR "${NET_IF}"
+# . /lib/functions/network.sh
+# network_flush_cache
+# network_find_wan NET_IF
+# network_get_ipaddr NET_ADDR "${NET_IF}"
+# echo "Current IP: ${NET_ADDR}"
+
+# ---
+# gets the ip from internet (works in dhpc mode)
+# sed explained:
+#   -n Do not print the input
+#   -r Regex extended (don't need to escape the parentheses)
+#   \1 The first capture group
+#   /p Print if a replacement was done
+NET_ADDR=$(curl -s -X GET "https://1.1.1.1/cdn-cgi/trace" | sed -nr 's/ip=(\d.+)/\1/p')
 echo "Current IP: ${NET_ADDR}"
 
 # ---
